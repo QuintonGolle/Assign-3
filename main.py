314a2e28295fd30c8e51d85eda334b9d42c5c3b4
QuintonGolle <123510970+QuintonGolle@users.noreply.github.com>
2023-04-03 19:27:48 -0400

    Game setup and commented pseudocode

---
+++ main.py
@@ -0,0 +1,57 @@
+import pygame
+from sys import exit
+
+pygame.init()
+
+screen = pygame.display.set_mode((800,400))
+
+clock = pygame.time.Clock()
+
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+            
+            
+            
+    pygame.display.update()
+    clock.tick(60)
+
+
+
+
+
+
+
+#structure of game:
+
+#set up player movement controller (left, right, jump)
+#set up the attack animation (played on left mouse click)
+
+#set wave, plr_lvl variable to 1 at the beginning of program
+#show the current wave at the top left of the screen
+#show "[LEVEL] Knight [HP]" above the characters head
+
+#summon monsters on random platforms equal to the current wave + 2 (up for change)
+#add 25 to player xp when an enemy is killed by the player
+#the enemies deal 5 damage to the player, and 5 to the castle
+#
+#formula for calculating level
+# current_exp = 200
+# def experience(level):
+# 
+#     exp = 0
+#     for i in range(1, level):
+#         exp += (i + 300 * 2 ** (i / 7)) // 4
+#     if current_exp >= exp:
+#         level = level + 1
+#     return int(level)
+# current_lvl = experience(3)
+
+#once there are no more enemies left in a single wave, begin the next wave
+# continute this process until the player, or the castle has died
+#quit the game
+
