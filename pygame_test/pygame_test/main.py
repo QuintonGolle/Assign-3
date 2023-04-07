68e8bdc47a67a89825eee10b040cc1fee1ac1c2b
QuintonGolle <123510970+QuintonGolle@users.noreply.github.com>
2023-04-07 18:48:18 -0400

    Game finishes minus some displayed text

--- pygame_test/pygame_test/main.py
+++ pygame_test/pygame_test/main.py
@@ -5,6 +5,7 @@ from random import randint
 
 #starts up the program (sets up everything that needs to be set up behind the scenes)
 pygame.init()
+pygame.font.init()
 
 
 screen_height = 400
@@ -21,8 +22,26 @@ clock = pygame.time.Clock()
 background = pygame.image.load("pygame_test/assets/background.png")
 castle = pygame.image.load("pygame_test/assets/castle.png")
 platform = pygame.image.load("pygame_test/assets/road.png")
-player = pygame.image.load("pygame_test/assets/player.png")
-enemy = pygame.image.load("pygame_test/assets/enemy.png")
+player_sprites = [pygame.image.load("pygame_test/assets/player.png"),
+          pygame.image.load("pygame_test/assets/player_att_1.png"),
+          pygame.image.load("pygame_test/assets/player_att_2.png")]
+player = player_sprites[0]
+enemy_sprites = [pygame.image.load("pygame_test/assets/enemy.png"),
+                 pygame.image.load("pygame_test/assets/en_att_anim_1.png"),
+                 pygame.image.load("pygame_test/assets/en_att_anim_2.png")]
+enemy = enemy_sprites[0]
+
+lizard_death = pygame.mixer.Sound("pygame_test/assets/lizard_death.wav")
+lizard_death.set_volume(0.3)
+lizard_hurt = pygame.mixer.Sound("pygame_test/assets/lizard_hurt.mp3")
+lizard_hurt.set_volume(0.3)
+player_hurt = pygame.mixer.Sound("pygame_test/assets/player_hurt.wav")
+player_hurt.set_volume(0.6)
+background_music = pygame.mixer.music.load("pygame_test/assets/background.mp3")
+pygame.mixer.music.play(-1,0.0)
+
+blue = (0,0,128)
+font = pygame.font.SysFont('freesansbold.ttf', 32)
 
 #the entity positions initially
 player_pos = [200, 376]
@@ -39,118 +58,188 @@ enemy_health = 3
 castle_health = 100
 enemies_left = 1
 player_gravity = 0
-att_cd = 0
+plr_att_cd = 0
+enemy_cd = 0
 player_dir = "right"
 enemy_dir = "left"
 enemies_to_spawn = 3
 new_wave = True
-
-active_enemies = []
-
-
+game_over = False
+kill_enemy = False
 
 
+def enemy_animate(enemy_cd, enemy):
+    global plr_health
+    if enemy_cd == 180:
+        #add first animation frame
+        enemy = enemy_sprites[1]
+        
+    if enemy_cd == 150:
+        enemy = enemy_sprites[2]
+        #add last animation frame
+        if plr_collision.colliderect(enemy_collision):
+            plr_health = plr_health - 20
+            player_hurt.play()
+            
+        #check for damage
+        
+    if enemy_cd == 110:
+        enemy = enemy_sprites[0]
+        #reset character to idle animation
+    # print(plr_health)
+    return enemy
 
+def player_animate(plr_att_cd, player):
+    global enemy_health
+    global kill_enemy
+    if plr_att_cd == 59:
+        #add first animation frame
+        player = player_sprites[1]
+        
+    if plr_att_cd == 50:
+        player = player_sprites[2]
+        #add last animation frame
+        if plr_collision.colliderect(enemy_collision):
+           enemy_health = enemy_health - 1
+           lizard_hurt.play()
+           if enemy_health <= 0:
+                enemy_pos[0] = 700
+                enemy_health = 3
+                lizard_death.play()
+            
+        #check for damage
+        
+    if plr_att_cd == 35:
+        player = player_sprites[0]
+        #reset character to idle animation
+    # print(plr_health)
+    return player
+
+    
+    
 #main game loop
 while True:
-    #runs every time an action is made in the game window
-    for event in pygame.event.get():
-        #checks if that action is trying to quit the game
-        if event.type == pygame.QUIT:
-            #quits the pygame instance and then kills the code with exit()
-            pygame.quit()
-            exit()
-    
-    
-    #puts all the surfaces on the screen with variable positions, also linked to collisions
-    screen.blit(background, (0,0))
-    screen.blit(castle, (0,-15))
-    
-    #setup the ground and the collision for it
-    screen.blit(platform, (190,376))
-    platform_rect = platform.get_rect(topleft = (190,376))
-    
-    enemy_collision = enemy.get_rect(midbottom = (enemy_pos[0],enemy_pos[1]))
-    screen.blit(enemy, enemy_collision)
-    
-    #set up the player and the collision for it
-    plr_collision = player.get_rect(midbottom = (player_pos[0],player_pos[1]))
-    screen.blit(player, plr_collision)
-    
-    
-    #checks if the player has collided with the ground (true/false)
-    onGround = plr_collision.colliderect(platform_rect)
-    
-    #makes an array of all the key presses
-    keys = pygame.key.get_pressed()
-    
-     #moves the player left when they press A
-    if keys[pygame.K_a] and player_pos[0] > 197:
-        player_pos[0] -= 3
-        if player_dir == "right":
-            player = pygame.transform.flip(player, True, False)
-            player_dir = "left"
-    
-    #moves the player right when they press D
-    if keys[pygame.K_d] and player_pos[0] < 668:
-        player_pos[0] += 3
-        if player_dir == "left":
-            player = pygame.transform.flip(player, True, False)
-            player_dir = "right"
-    
-    #makes the player jump when they press space as long as they are on the ground
-    if keys[pygame.K_SPACE] and onGround == True:
-        #makes the gravity negative so it moves the player up in a jumping fashion
-        player_gravity = -13
-        player_pos[1] += player_gravity
-    
-    #moves the player down at an amount that increases the longer they are in the air, only if they are not on the ground
-    if onGround == False:
-        player_gravity += 1
-        player_pos[1] += player_gravity
-    
-    #does the attack process if the right control key is pressed and the attack isnt on cooldown
-    if keys[pygame.K_RCTRL] and att_cd == 0:
-        slash = pygame.mixer.Sound("pygame_test/assets/slash.ogg")
-        slash.set_volume(0.3)
-        slash.play()        
-        #sets the cooldown to 1 second
-        att_cd = 60
-    
-    #lowers the attack cooldown every frame (60 times a second)
-    if att_cd > 0:
-        att_cd -= 1
-    
-    #calculates the distance from the player to the enemy
-    x_diff = enemy_pos[0] - player_pos[0]
-    y_diff = enemy_pos[1] - player_pos[1]
-    
-    #checkis if a player is far away, if they are then walk to the castle
-    if (x_diff > 40 or x_diff < -40) and (y_diff < 10 or y_diff > -10):
-        enemy_pos[0] -= 2
-        #changes the direction of the enemy image to correspond with movement direction
-        if enemy_dir == "right":
-            enemy = pygame.transform.flip(enemy, True, False)
-            enemy_dir = "left"
-    #if they are close, and in the negative direction, then walk in the negative direction
-    elif x_diff > 20:
-        enemy_pos[0] -= 2
-        if enemy_dir == "right":
-            enemy = pygame.transform.flip(enemy, True, False)
-            enemy_dir = "left"
-    #if they are close, and in the positive direction, then walk in the positive direction
-    elif x_diff < -20:
-        enemy_pos[0] += 2
-        if enemy_dir == "left":
-            enemy = pygame.transform.flip(enemy, True, False)
-            enemy_dir = "right"
-            
-    for i in range(enemies_to_spawn):
-        active_enemies.append(enemy.get_rect(midbottom = (randint(700, 900),376)))
+    if game_over == False:
+        #runs every time an action is made in the game window
+        for event in pygame.event.get():
+            #checks if that action is trying to quit the game
+            if event.type == pygame.QUIT:
+                #quits the pygame instance and then kills the code with exit()
+                pygame.quit()
+                exit()
+        
+        
+        #puts all the surfaces on the screen with variable positions, also linked to collisions
+        screen.blit(background, (0,0))
+        screen.blit(castle, (0,-15))
+        
+        #setup the ground and the collision for it
+        screen.blit(platform, (190,376))
+        platform_rect = platform.get_rect(topleft = (190,376))
+        
+        enemy_collision = enemy.get_rect(midbottom = (enemy_pos[0],enemy_pos[1]))
         screen.blit(enemy, enemy_collision)
         
-    
+        #set up the player and the collision for it
+        plr_collision = player.get_rect(midbottom = (player_pos[0],player_pos[1]))
+        screen.blit(player, plr_collision)
         
+        
+        #checks if the player has collided with the ground (true/false)
+        onGround = plr_collision.colliderect(platform_rect)
+        
+        #makes an array of all the key presses
+        keys = pygame.key.get_pressed()
+        
+        #moves the player left when they press A
+        if keys[pygame.K_a] and player_pos[0] > 197:
+            player_pos[0] -= 3
+            if player_dir == "right":
+                player = pygame.transform.flip(player, True, False)
+                player_dir = "left"
+        
+        #moves the player right when they press D
+        if keys[pygame.K_d] and player_pos[0] < 668:
+            player_pos[0] += 3
+            if player_dir == "left":
+                player = pygame.transform.flip(player, True, False)
+                player_dir = "right"
+        
+        #makes the player jump when they press space as long as they are on the ground
+        if keys[pygame.K_SPACE] and onGround == True:
+            #makes the gravity negative so it moves the player up in a jumping fashion
+            player_gravity = -13
+            player_pos[1] += player_gravity
+        
+        #moves the player down at an amount that increases the longer they are in the air, only if they are not on the ground
+        if onGround == False:
+            player_gravity += 1
+            player_pos[1] += player_gravity
+        
+        #does the attack process if the right control key is pressed and the attack isnt on cooldown
+        if keys[pygame.K_RCTRL] and plr_att_cd == 0:
+            slash = pygame.mixer.Sound("pygame_test/assets/slash.ogg")
+            slash.set_volume(0.3)
+            slash.play()        
+            #sets the cooldown to 1 second
+            plr_att_cd = 60
+            
+            
+        
+        #lowers the attack cooldown every frame (60 times a second)
+        if plr_att_cd > 0:
+            player = player_animate(plr_att_cd, player)
+            plr_att_cd -= 1
+            
+        
+        #calculates the distance from the player to the enemy
+        x_diff = enemy_pos[0] - player_pos[0]
+        y_diff = enemy_pos[1] - player_pos[1]
+        
+        #checkis if a player is far away, if they are then walk to the castle
+        if (x_diff > 40 or x_diff < -40) and (y_diff < 10 or y_diff > -10):
+            enemy_pos[0] -= 2
+            #changes the direction of the enemy image to correspond with movement direction
+            if enemy_dir == "right":
+                enemy = pygame.transform.flip(enemy, True, False)
+                enemy_dir = "left"
+        #if they are close, and in the negative direction, then walk in the negative direction
+        elif x_diff > 20:
+            enemy_pos[0] -= 2
+            if enemy_dir == "right":
+                enemy = pygame.transform.flip(enemy, True, False)
+                enemy_dir = "left"
+        #if they are close, and in the positive direction, then walk in the positive direction
+        elif x_diff < -20:
+            enemy_pos[0] += 2
+            if enemy_dir == "left":
+                enemy = pygame.transform.flip(enemy, True, False)
+                enemy_dir = "right"
+        
+        
+        
+        if enemy_cd == 0 and ((x_diff > 15 and x_diff < 40)  or (x_diff < -15 and x_diff > -40)):
+            enemy_cd = 180
+        
+        # print(plr_att_cd)
+        #print(near)
+        # print(enemy_health)
+            
+        if enemy_cd > 0:
+            enemy = enemy_animate(enemy_cd, enemy)
+            enemy_cd -= 1
+            if plr_health <= 0:
+                game_over = True
+                
+        if enemy_pos[0] < 197:
+            castle_health = castle_health - 10
+            enemy_pos[0] = 700
+        
+        print(castle_health)
+    else:
+        text = font.render('Game Over', False, (0, 0, 0))
+        screen.blit(text, (360, 200))
+
         
 
     #updates the display
