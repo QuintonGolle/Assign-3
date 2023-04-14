e0be0b8e4c3c78a3a263da603e260b765e49d598
QuintonGolle <123510970+QuintonGolle@users.noreply.github.com>
2023-04-14 09:59:29 -0400

    Added rest of comments, made the castle dying end the game

--- pygame_test/pygame_test/main.py
+++ pygame_test/pygame_test/main.py
@@ -7,7 +7,7 @@ from random import randint
 pygame.init()
 pygame.font.init()
 
-
+#variables for screen size
 screen_height = 400
 screen_width = 672
 
@@ -18,7 +18,7 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 #initializes the clock function
 clock = pygame.time.Clock()
 
-#sets assets up
+#sets assets up (gives every image a variable)
 background = pygame.image.load("pygame_test/assets/background.png")
 castle = pygame.image.load("pygame_test/assets/castle.png")
 platform = pygame.image.load("pygame_test/assets/road.png")
@@ -31,6 +31,7 @@ enemy_sprites = [pygame.image.load("pygame_test/assets/enemy.png"),
                  pygame.image.load("pygame_test/assets/en_att_anim_2.png")]
 enemy = enemy_sprites[0]
 
+#sets up all the sound, sets volume and gives each sound a variable
 lizard_death = pygame.mixer.Sound("pygame_test/assets/lizard_death.wav")
 lizard_death.set_volume(0.3)
 lizard_hurt = pygame.mixer.Sound("pygame_test/assets/lizard_hurt.mp3")
@@ -40,12 +41,12 @@ player_hurt.set_volume(0.6)
 background_music = pygame.mixer.music.load("pygame_test/assets/background.mp3")
 pygame.mixer.music.play(-1,0.0)
 
+#chooses font size, font and color
 blue = (0,0,128)
 font = pygame.font.SysFont('freesansbold.ttf', 32)
 diff_font = pygame.font.SysFont('freesansbold.ttf', 18)
-# remember to add this for player health
 
-
+#a function to animate the enemy attack based on the cooldown of the attack
 def enemy_animate(enemy_cd, enemy):
     global plr_health
     if enemy_cd == 180:
@@ -54,19 +55,17 @@ def enemy_animate(enemy_cd, enemy):
         
     if enemy_cd == 150:
         enemy = enemy_sprites[2]
-        #add last animation frame
+        #add last animation frame, all effects happen if collision between the enemy and player happen in this frame (damage to player, sound effects)
         if plr_collision.colliderect(enemy_collision):
             plr_health = plr_health - 20
             player_hurt.play()
-            
-        #check for damage
         
     if enemy_cd == 110:
         enemy = enemy_sprites[0]
         #reset character to idle animation
-    # print(plr_health)
     return enemy
 
+#function that animated player attack based on the player attack cooldown
 def player_animate(plr_att_cd, player):
     global enemy_health
     global kill_enemy
@@ -77,23 +76,24 @@ def player_animate(plr_att_cd, player):
         
     if plr_att_cd == 50:
         player = player_sprites[2]
-        #add last animation frame
+        #add last animation frame, checks for collision between player and enemy
         if plr_collision.colliderect(enemy_collision):
+           #if collided, subract from enemy health, play hurt sound
            enemy_health = enemy_health - 1
            lizard_hurt.play()
+           #if the enemy dies, reset them to their origional position off screen, reset health and add to player score
            if enemy_health <= 0:
                 enemy_pos[0] = 700
                 enemy_health = 3
                 lizard_death.play()
                 score = score + 1 
-
         
     if plr_att_cd == 35:
         player = player_sprites[0]
         #reset character to idle animation
-    # print(plr_health)
     return player
 
+#shows the controls on screen for 3 seconds before the game starts
 for i in range(180):
     
     titlescreen_1  = font.render('A = Left', True,(255,255,255))
@@ -135,9 +135,10 @@ while True:
     #the entity positions initially
     player_pos = [200, 376]
     enemy_pos = [700, 376]
-
+    #the main game while loop      
     while True:
-        if game_over == False:
+          #checks if the game has ended or not
+          if game_over == False:
             #runs every time an action is made in the game window
             for event in pygame.event.get():
                 #checks if that action is trying to quit the game
@@ -233,19 +234,14 @@ while True:
                 if enemy_dir == "left":
                     enemy = pygame.transform.flip(enemy, True, False)
                     enemy_dir = "right"
-            
-            
-            
+            #if the player is within a certain range, set the cooldown to 180 which will start the attack function
             if enemy_cd == 0 and ((x_diff > 15 and x_diff < 40)  or (x_diff < -15 and x_diff > -40)):
                 enemy_cd = 180
-            
-            # print(plr_att_cd)
-            #print(near)
-            # print(enemy_health)
-                
+            #check if enemy has detected the time to attack    
             if enemy_cd > 0:
                 enemy = enemy_animate(enemy_cd, enemy)
                 enemy_cd -= 1
+                #if enemy has attacked and player health is 0 or below, end the game (put game over on the screen for 5 seconds, set game over to true)    
                 if plr_health <= 0:
                     for i in range(300):
                         t_seconds = i / 60
@@ -258,14 +254,26 @@ while True:
                         pygame.display.update()
                         #utilizes the clock function to make sure the while loop only runs 60 times a second (60fps)
                         clock.tick(60)
-                    
+            #check if enemy has reached the castle, if it has, subract from castle health        
             if enemy_pos[0] < 197:
                 castle_health = castle_health - 10
+                if castle_health <= 0:
+                    for i in range(300):
+                        t_seconds = i / 60
+                        text = font.render('Game Over', False, (0, 0, 0))
+                        timer = font.render('Restarting...' , False, (0, 0, 0))
+                        screen.blit(text, (360, 200))
+                        screen.blit(timer, (360, 220))
+                        game_over = True
+                        #updates the display
+                        pygame.display.update()
+                        #utilizes the clock function to make sure the while loop only runs 60 times a second (60fps)
+                        clock.tick(60)
                 enemy_pos[0] = 700
-                
+        # if game over is not false, break out of the main game loop and reset the game        
         else:
             break    
-        
+        #display player health, castle health, and game score on screen
         health = font.render('Castle health '+ str(castle_health), True, (0, 0, 0))
         screen.blit(health, (0,0))
 
